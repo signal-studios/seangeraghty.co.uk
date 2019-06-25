3ce3ee4582312e1763bb0d10c41a94ffd64b8c8a
Sean <sean@geraghty.co>
2019-06-25 11:56:45 +0100

    Updated the Florence build

--- florence/scripts.js
+++ florence/scripts.js
@@ -1,6 +1,4 @@
-instafetch.init({
-  accessToken: '7434904024.1677ed0.de0ed5c92b2e4a879bb2adb3c2d9a848',
-  target: 'instagram_feed',
-  numOfPics: 4,
-  caption: false
-});
+function toggleMenu() {
+  document.getElementById("menu").classList.toggle("show");
+  document.getElementById("menu_button").classList.toggle("hide");
+}
